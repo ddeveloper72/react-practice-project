af24e2b4af548e95aa40836141eaae37f51e9269
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-23 20:19:56 +0100

    added css classes

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,19 +1,23 @@
- import React from "react";
+import React from "react";
+import Card from "../UI/Card";
+import './AddUsers.css';
 
- const AddUser = props => {
-     const AddUserHandler = (event) => {
-         event.preventDefault();
-     };
+const AddUser = props => {
+    const AddUserHandler = (event) => {
+        event.preventDefault();
+    };
 
     return (
         <form onSubmit={AddUserHandler}>
-            <label htmlFor="name">Name:</label>
-            <input id="name" type="text" ></input>
-            <label htmlFor="age">Age (years)</label>
-            <input id="age" type="number" ></input>
-            <button type="submit">Submit</button>
+            <Card className='name-card'>
+                <label htmlFor="name">Name:</label>
+                <input id="name" type="text"></input>
+                <label htmlFor="age">Age (years)</label>
+                <input id="age" type="number"></input>
+                <button className="name-card__button" type="submit">Submit</button>
+            </Card>
         </form>
     );
- };
+};
 
- export default AddUser;
+export default AddUser;
