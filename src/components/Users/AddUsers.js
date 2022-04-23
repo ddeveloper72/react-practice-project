9fd3e235fc33af22d4760b91b255c725040e41ef
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-23 11:38:56 +0100

    setup a form

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -2,7 +2,15 @@
 
  const AddUser = props => {
 
-    return ();
+    return (
+        <form>
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" ></input>
+            <label htmlFor="age">Age (years)</label>
+            <input id="age" type="number" ></input>
+            <button type="submit">Submit</button>
+        </form>
+    );
  };
 
  export default AddUser;
