3fe142bced151afed8e9a6f7c9b7e25cf1650f7c
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-24 12:03:24 +0100

    define function for setting the new value from the input

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -5,17 +5,22 @@ import Styles from './AddUsers.module.css';
 
 const AddUser = props => {
     // define constants for state items
-const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredName, setEnteredName] = useState('');
 
     const AddUserHandler = (event) => {
         event.preventDefault();
+    };
+
+    // define function for setting the new value from the input
+    const nameChangeHandler = (event) => {
+        setEnteredName(event.target.value);
     };
 
     return (
         <Card className={`${Styles['name-card']}`}>
             <form onSubmit={AddUserHandler}>
                 <label htmlFor="name">Name:</label>
-                <input id="name" type="text"></input>
+                <input id="name" type="text" onChange={nameChangeHandler}></input>
                 <label htmlFor="age">Age (years)</label>
                 <input id="age" type="number"></input>
                 <Button type="submit">Add Goal</Button>
