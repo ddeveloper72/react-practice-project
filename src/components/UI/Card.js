ddaba0e6149bbbea1cedf773568d10f2ef7e35d2
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-24 11:02:17 +0100

    inject custom styles along wiht inhereted props style

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import classes from './Card.module.css';
+import Styles from './Card.module.css';
 
 const Card = (props) => {
     
-    return <div className={classes.card}>{props.children}</div>
+    return <div className={`${Styles.card} ${props.className}`}>{props.children}</div>
 }
 
 export default Card;
