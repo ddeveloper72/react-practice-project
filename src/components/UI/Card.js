d6ed95651df85bb146af376706be42d48a66926b
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-24 10:02:02 +0100

    added import for css card module

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './Card.css';
+import classes from './Card.module.css';
 
 const Card = (props) => {
-    // remember to include a space when joining two strings so they read as separate strings
-    const classes = 'card ' + props.className;
-
-    return <div className={classes}>{props.children}</div>
+    
+    return <div className={classes.card}>{props.children}</div>
 }
 
 export default Card;
