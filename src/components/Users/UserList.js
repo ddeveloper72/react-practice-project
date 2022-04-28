b852e5f2e1de2a35f5c4530de0257c7f473085fd
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-28 23:04:28 +0100

    added style class imports

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "../UI/Card";
+import Styles from './UserList.module.css';
 
 const UserList = props => {
     return (
-        <Card>
+        <Card className={Styles.users}>
             <ul>
                 {props.users.map((user) => (
                     <li>{user.name} ({user.age} years old)</li>
