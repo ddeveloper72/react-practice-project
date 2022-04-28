b6b2a699d684c689e35dc81bf96fff3966e91110
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-28 23:09:56 +0100

    add output for UserList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import AddUser from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UserList />
     </div>
 
   );
