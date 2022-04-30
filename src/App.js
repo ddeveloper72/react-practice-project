20275b4a9cbb6e843576f8cd3f678ed252a7c7a9
ddeveloper72 <duncan.niamh@gmail.com>
2022-04-30 10:05:59 +0100

    create addUserhandler function WIP

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import React, {useState} from 'react';
 import AddUser from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
-import { useState } from 'react';
 
 function App() {
   // create a state for working with userList
   const [userList, setUserList] = useState([]);
 
+  const AddUserHandler = (userName, userAge) => {
+    // expect 2 arguments, name and age
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={AddUserHandler}/>
       {/* render list with and empty starting array */}
-      <UserList users={[]} />
+      <UserList users={userList} />
 
     </div>
 
