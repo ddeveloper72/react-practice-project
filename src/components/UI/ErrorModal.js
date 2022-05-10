37b9d558357d231092d5c057eb3729072d83f12e
ddeveloper72 <duncan.niamh@gmail.com>
2022-05-10 10:56:05 +0100

    added a path name correction to styles

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
-import Styles from "ErrorModal.module.css";
+import Styles from "./ErrorModal.module.css";
 
 const ErrorModal = props => {
 
