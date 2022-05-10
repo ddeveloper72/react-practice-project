1381af238b94146f638473ecea23fe1411843844
ddeveloper72 <duncan.niamh@gmail.com>
2022-05-10 10:03:13 +0100

    added modal html elemets WIP

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,9 +4,15 @@ import Card from "./Card";
 const ErrorModal = props => {
 
     return <Card>
-        
+        <header>
+            <h2></h2>
+        </header>
+        <div>
+            <p></p>
+        </div>
+        <footer>
 
-
+        </footer>
     </Card>
 };
 
