cd2699e99ba4e85cfef6cd5a7a523cf762661314
ddeveloper72 <duncan.niamh@gmail.com>
2022-05-10 15:14:29 +0100

    added bacdrop style for modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,17 +6,20 @@ import Styles from "./ErrorModal.module.css";
 const ErrorModal = props => {
 
     return (
-        <Card className={Styles.modal}>
-            <header className={Styles.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={Styles.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={Styles.actions}>
-                <Button>Ok</Button>
-            </footer>
-        </Card>
+        <div>
+            <div className={Styles.backdrop} />
+            <Card className={Styles.modal}>
+                <header className={Styles.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={Styles.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={Styles.actions}>
+                    <Button>Ok</Button>
+                </footer>
+            </Card>
+        </div>
     )
 };
 
